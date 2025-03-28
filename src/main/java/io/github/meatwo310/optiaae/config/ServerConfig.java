640627605a84b3f4c1586883233bf54d35531037
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-03-29 00:33:23 +0900

    コンフィグのコメント拡張+上限ぶち上げ

--- src/main/java/io/github/meatwo310/optiaae/config/ServerConfig.java
+++ src/main/java/io/github/meatwo310/optiaae/config/ServerConfig.java
@@ -9,10 +9,15 @@ public class ServerConfig {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
     public static final ForgeConfigSpec.IntValue AUTO_STOCK_TICKS = BUILDER
-            .comment("Ticks between Auto Stock upgrade checks. " +
-                    "Smaller values make it more responsive, and larger values greatly improve performance. " +
-                    "Set to 0 completely disables the upgrade functionality.")
-            .defineInRange("autoStockTicks", 20, 0, 20);
+            .comment("""
+                    Ticks between Auto Stock upgrade checks.
+                    Smaller values make it more responsive, and larger values greatly improve performance.
+                    0: Completely disables the upgrade function.
+                    1: Checks every tick. AdvancedAE's default behavior.
+                    20: Checks every second. Default value.
+                    """
+            )
+            .defineInRange("autoStockTicks", 20, 0, Integer.MAX_VALUE);
 
     public static final ForgeConfigSpec SPEC = BUILDER.build();
 }
